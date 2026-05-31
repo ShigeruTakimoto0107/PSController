3cff03069c3a1dcb865bcb70d72cbd7fbd00415c
ShigeruTakimoto0107 <shigeru_takimoto@ybb.ne.jp>
2026-06-01 06:52:56 +0900

    fix: fix TranscriptOpenCommand display and remove unnecessary PSReadLine module unload
    - Remove unnecessary Remove-Module PSReadLine call
    - Remove unnecessary BeginWait/WaitUntilMatched/PromptWritten block
    - Add Console.WriteLine() before Start-Transcript to ensure output appears on new line after prompt
    - Add using System; to TranscriptOpenCommand.cs
    --------------------------
    fix: TranscriptOpenCommandの表示修正とPSReadLineモジュール削除処理を除去
    
    - 不要なRemove-Module PSReadLine の呼び出しを削除
    - 不要なBeginWait/WaitUntilMatched/PromptWrittenブロックを削除
    - Start-Transcript前にConsole.WriteLine()を追加しプロンプトの次の行に出力されるよう修正
    - TranscriptOpenCommand.csにusing System;を追加

--- src/Core/VersionInfo.cs
+++ src/Core/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace PowerShellController {
           public const string ProgramName = "PowerShellController";
           public const string Version     = "0.0.1";
           public const string Copyright   = "(C) 2026 Kolog898";
-          public const string BuildDate   = "2026-05-31";
-          public const string GitVersion  = "9aede2b";
+          public const string BuildDate   = "2026-06-01";
+          public const string GitVersion  = "2d5a168";
       }
 }
